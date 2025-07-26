e915b627fa9458592a34d2b332fb6a60e00581f7
Shahzeb13 <razashahzaib119@gmail.com>
2025-07-26 17:18:13 +0500

    Update GroupAnagram.py

--- GroupAnagram.py
+++ GroupAnagram.py
@@ -51,3 +51,15 @@ def groupAnagram(strs):
 
 res = groupAnagram(["act","pots","tops","cat","stop","hat"])
 print(res)
+
+
+
+//Explanation
+Anagrams are the words that have same frequency and same length i.e. cat and act
+Problem Statement:
+We have to give a function an array of Anagrams and then we want that fucntion to return a list of lists i.e [[hat] , [tops , pots , stop] , [act , cat]]
+Here is the algrothim for solving this problem.
+1. First of all, I have to create an empty list to store the grouped anagrams
+2. I have to use two loops to compare each anagram with its fellow neighbours.
+3. I will also use set() to make sure that once a jth word is matched for the ith term! we will group them together to avoid using that same word comparision with another Word.
+3. Simple we will iterate J+1th term for every ith term skipping those who are already used. AFter that we wiull return the list in reversed.
