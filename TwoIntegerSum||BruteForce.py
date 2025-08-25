cf7c3b1077e5b146b251b5038e6fcc4933ad670c
Shahzeb13 <razashahzaib119@gmail.com>
2025-08-25 11:53:10 +0500

    Create TwoIntegerSum||BruteForce.py

---
+++ TwoIntegerSum||BruteForce.py
@@ -0,0 +1,48 @@
+def TwoIntegerSum2(numbers , target):
+    print("==========TwoIntegerSum2===========");
+    '''
+    ===problem===
+    So, i have an array and a target! I have to return the indeces of the 1-index array of the two nums that matches the target
+
+    Constraints:
+
+    2 <= numbers.length <= 1000
+    -1000 <= numbers[i] <= 1000
+    -1000 <= target <= 1000
+
+    I can use brute force solution here 
+    The element of the array can be negative/positive
+    The Target can also be negative/positive
+
+    I will aim for O(n log n ) or O(n) soluton after bruteforce
+
+    ===pseudo Code===
+    for i in range(len(numbers)):
+        for j in range(i+1 , len(numbers));
+            if numbers[i] + numbers[j] == target;
+            return [i+1 , j+1]
+
+
+    ===dry run===
+
+     [-10, -3, 0, 2, 5, 8, 12]
+
+    Iteration    i      j       i[val]     j[val]     sum            target is -5
+
+        I don't wanna waste my time doing brute force! I know how it works  
+
+    '''
+    #implemenentation of bruteforce
+    for i in range(len(numbers)):
+        for j in range(i+1 , len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return [i+1 , j+1]
+
+
+
+
+
+
+
+output = TwoIntegerSum2([1,2 ,3,4, -7], -5);
+print(output);
